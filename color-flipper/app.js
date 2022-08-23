d29940d900432ba703fd34911b1b3dbfa39ee4d9
Hershell Koger <hersheykoger@yahoo.com>
2022-08-23 12:08:00 -0400

    Add event listener to btn

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -2,3 +2,7 @@ const colors = ["green", "red", "rgb(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    console.log(document.body);
+})

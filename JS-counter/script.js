394a05f7cf6dc3c75bc3c833b2880ad1ca5bb048
Hershell Koger <hersheykoger@yahoo.com>
2022-08-28 06:20:32 -0400

    Add if statement checking for 'subtract' class on button click, then decreasing count - output value will now equal count

--- JS-counter/script.js
+++ JS-counter/script.js
@@ -1,3 +1,6 @@
+
+let count = 0;
+
 const output = document.querySelector('#output');
 
 const btns = document.querySelectorAll('.btn');
@@ -6,7 +9,8 @@ btns.forEach(function(btn){
     btn.addEventListener("click", function(e) {
         const styles = e.currentTarget.classList;
         if (styles.contains('subtract')) {
-            output--;
+            count--;
         }
+        output.textContent = count;
     })
 })

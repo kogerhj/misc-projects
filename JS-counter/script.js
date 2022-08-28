11b07fdf19c09a0b72c45945b9d4d0c01ba3de2c
Hershell Koger <hersheykoger@yahoo.com>
2022-08-28 04:58:52 -0400

    Use forEach method on btns binding and add eventListener with callback function - use event target to access classList

--- JS-counter/script.js
+++ JS-counter/script.js
@@ -1,3 +1,12 @@
 const output = document.querySelector('#output');
 
 const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('subtract')) {
+            output--;
+        }
+    })
+})

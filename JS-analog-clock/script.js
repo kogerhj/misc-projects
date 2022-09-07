db5b19c5de534ca59214c1ea92ae299580bbfe42
Hershell Koger <hersheykoger@yahoo.com>
2022-09-06 20:13:01 -0400

    Create set Clock function and create binding for 'new Date()'

--- JS-analog-clock/script.js
+++ JS-analog-clock/script.js
@@ -4,3 +4,7 @@ const hourHand = document.querySelector('[data-hour-hand]');
 const minuteHand = document.querySelector('[data-minute-hand]');
 const secondHand = document.querySelector('[data-second-hand]');
 
+function setClock() {
+    const currentDate = new Date();
+    
+}

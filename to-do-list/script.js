e661b9b42ad5185db2f4c31e8a67ec6adbc036fd
Hershell Koger <hersheykoger@yahoo.com>
2022-09-11 06:38:31 -0400

    Create binding currentTasks for node list of delete buttons - use this node list and and event listener function to remove list items upon clicking delete button

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,12 +1,27 @@
 let inputSel = document.querySelector("[data-input]");
 let addButton = document.querySelector("[data-button]");
 let taskLine = document.querySelector("[data-tasks]");
+let deleteButton = document.querySelector("[data-delete]");
 
 addButton.addEventListener("click", () => {
-    taskLine.innerHTML = `
+    if(inputSel.value === "") return;
+
+    taskLine.innerHTML += `
     <div class="new-task" id="new-task" data-new-task>
         <span>${inputSel.value}</span>
-        <button>X</button>
+        <button class="delete" data-delete>X</button>
     </div>`
-})
+
+    let currentTasks = document.querySelectorAll(".delete");
+    
+    for (let i = 0; i < currentTasks.length; i++) {
+        currentTasks[i].onclick = function() {
+            this.parentNode.remove();
+        }
+        }
+    }
+    
+)
+
+
 

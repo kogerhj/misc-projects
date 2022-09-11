2fbbbc18c7a7aacae24a64c48fb1e7d674801ba1
Hershell Koger <hersheykoger@yahoo.com>
2022-09-11 05:13:07 -0400

    Create eventlistener for the Add button to inject the contents of the input field into the new task div

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,3 +1,7 @@
 let inputSel = document.querySelector("[data-input]");
 let addButton = document.querySelector("[data-button]");
 let taskLine = document.querySelector("[data-tasks]");
+
+addButton.addEventListener("click", () => {
+    taskLine.innerHTML = `${inputSel.value}`;
+})
